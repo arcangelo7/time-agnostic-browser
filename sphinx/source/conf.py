aaa1453005ae0683f8e7ad989b13f593ca667b0d
arcangelo7 <arcangelomas@gmail.com>
2021-06-01 10:44:31 +0200

    .nojekyll

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -28,7 +28,8 @@ author = 'Arcangelo Massari'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode'
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
