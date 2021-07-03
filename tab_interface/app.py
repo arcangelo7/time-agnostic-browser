2f0a16efdb29411f6fa847ae1a0f2e49cd9cc555
arcangelo7 <arcangelomas@gmail.com>
2021-07-03 14:42:34 +0200

    human readable: generation time, predicates. Repeated res. open world, config with base_uri

--- tab_interface/app.py
+++ tab_interface/app.py
@@ -5,31 +5,23 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 import urllib
 
 from time_agnostic_browser.agnostic_entity import AgnosticEntity
-from time_agnostic_browser.agnostic_query import AgnosticQuery
+from time_agnostic_browser.agnostic_query import AgnosticQuery, CONFIG_PATH
 from time_agnostic_browser.support import FileManager, _to_nt_sorted_list, _to_dict_of_nt_sorted_lists
 from time_agnostic_browser.sparql import Sparql
 
 from pprint import pprint
 
+CONFIG_PATH = "./tab_interface/config.json"
+
 app = Flask(__name__)
 app.secret_key = b'\x94R\x06?\xa4!+\xaa\xae\xb2\xf3Z\xb4\xb7\xab\xf8'
+config = FileManager(CONFIG_PATH).import_json()
 
-def human_readable_date(date:str) -> str:
+def get_human_readable_date(date:str) -> str:
     datetime_obj = parser.parse(date).replace(tzinfo=None)
-    return str(datetime_obj)
+    return datetime_obj.strftime("%d %B %Y, %H:%M:%S")
 
-@app.route("/")
-def home():
-    return render_template("home.jinja2")
-
-@app.route("/entity/<path:res>")
-def entity(res):
-    agnostic_entity = AgnosticEntity(res=res, related_entities_history=False)
-    try:
-        history = agnostic_entity.get_history()
-    except urllib.error.URLError:
-        flash("There are connection problems with the database.")
-        return redirect(url_for("home"))
+def get_human_readable_history(history:dict) -> dict:
     history = _to_dict_of_nt_sorted_lists(history)
     human_readable_history = dict()
     for uri, snapshots in history.items():
@@ -47,14 +39,33 @@ def entity(res):
                 p = s_p[1]
                 o = literal[1].replace('"', '') if len(literal) > 1 else s_p[2]
                 list_of_lists.append([s, p, o])
-            human_readable_snapshot = human_readable_date(snapshot)
+            human_readable_snapshot = get_human_readable_date(snapshot)
             human_readable_history.setdefault(uri, dict())
             human_readable_history[uri][human_readable_snapshot] = list_of_lists
+    return human_readable_history
+
+@app.route("/")
+def home():
+    return render_template("home.jinja2")
+
+@app.route("/entity/<path:res>")
+def entity(res):
+    agnostic_entity = AgnosticEntity(res=res, related_entities_history=False)
+    try:
+        history = agnostic_entity.get_history()
+    except urllib.error.URLError:
+        flash("There are connection problems with the database.")
+        return redirect(url_for("home"))
+    human_readable_history = get_human_readable_history(history)
     if human_readable_history:
         return render_template("entity.jinja2", res=res, history=human_readable_history)
     else:
-        flash("I do not have information about that in my data.")
+        flash("I do not have information about that entity in my data.")
         return redirect(request.referrer)
+
+@app.route("/get_config")
+def get_config():
+    return config
 
 if __name__ == "__main__":
     app.run(debug=True)
--- tab_interface/static/js/custom.js
+++ tab_interface/static/js/custom.js
@@ -10,13 +10,43 @@ function isURI(string) {
 }
 
 function transformEntitiesInLinks(){
-    $(".tripleObject").each(function(){
-        tripleMember = $(this).html()
-        if (isURI(tripleMember)) {
-            $(this).wrapInner(`<a href='' class='entity'></a>`)
-        }
+    config = $.get("/get_config", function(data){
+        base_urls = data["base_urls"]
+        $(".tripleObject").each(function(){
+            tripleMemberElement = $(this)
+            tripleMember = $(this).html()
+            if (base_urls.length) {
+                $.each(base_urls, function(i, base_url){
+                    if (tripleMember.includes(base_url)){
+                        $(tripleMemberElement).wrapInner(`<a href='' class='entity'></a>`)
+                    }        
+                });
+            } else {
+                if (isURI(tripleMember)) {
+                    $(this).wrapInner(`<a href='' class='entity'></a>`)
+                }    
+            }
+        });
     });
-} 
+}
+
+function showHumanReadablePredicate(){
+    $(".triplePredicate").each(function(){
+        original_uri = $(this).html()
+        slash = $(this).html().split("/")
+        no_slash = slash[slash.length - 1]
+        hashtag = no_slash.split("#")
+        no_hashtag = hashtag[hashtag.length - 1]
+        camelCase = no_hashtag.replace(/^[a-z]|[A-Z]/g, function(v, i) {
+            return i === 0 ? v.toUpperCase() : " " + v.toLowerCase();
+        });  
+        $(this)
+            // .attr("data-toggle", "tooltip")
+            // .attr("data-placement", "right")
+            .attr("title", original_uri)
+            .html(camelCase)
+    });
+}
 
 function resolveEntity(res){
     window.location.href = `/entity/${res}` 
@@ -40,6 +70,8 @@ $(document).on("click", "a.entity", function(e){
 });
 
 $(function() {
+    showHumanReadablePredicate();
     transformEntitiesInLinks();
+    $('[data-toggle="tooltip"]').tooltip()
     $('.middle-line').first().remove();
 });
